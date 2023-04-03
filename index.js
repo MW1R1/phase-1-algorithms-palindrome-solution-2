4ef638b90a2fd3125d58ca63e785cd53608f4967
MW1R1 <nickmwirigi58@gmail.com>
2023-04-03 19:28:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+// Write your algorithm here
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
 
+  return true;
+}
 /* 
   Add your pseudocode here
 */
-
+/*if (word[startIndex] !== word[endIndex]) {
+  return false;
+}*/
 /*
   Add written explanation of your solution here
 */
-
+//isPalindrome function to check wheteher word is palindrome
+//split word and reverse then return true or false
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log(isPalindrome("madam"));  // true
+  console.log(isPalindrome("robot"));  // false
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
